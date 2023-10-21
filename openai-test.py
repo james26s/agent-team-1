95161bbfb55945b37e903822e43d8ad34d0e10f5
Shen <James.Shen@bain.com>
2023-10-21 19:08:27 -0400

    update test and requirements

--- openai-test.py
+++ openai-test.py
@@ -1,13 +1,33 @@
-import os
+import json
 import openai
-openai.api_key = os.getenv("OPENAI_API_KEY")
 
-completion = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
-  ]
-)
+# Read the configuration from the JSON file
+with open("OAI_CONFIG_LIST_B", "r") as config_file:
+    config_data = json.load(config_file)
 
-print(completion.choices[0].message)
+# Specify the model name you want to use
+desired_model_name = "gpt-3.5-turbo"
+
+# Find the API key associated with the desired model name
+api_key = None
+for model_config in config_data:
+    if model_config["model"] == desired_model_name:
+        api_key = model_config["api_key"]
+        break
+
+if api_key:
+    # Set the API key for the OpenAI Python client
+    openai.api_key = api_key
+
+    # Make the API call using the specified model
+    completion = openai.ChatCompletion.create(
+      model=desired_model_name,
+      messages=[
+        {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+        {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+      ]
+    )
+
+    print(completion.choices[0].message)
+else:
+    print(f"No API key found for model: {desired_model_name}")
